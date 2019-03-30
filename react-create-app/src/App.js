5ca2e92d71d8f39ea03e2645d50ed068aa5f683b
Be Broide <be.broide@gmail.com>
2019-03-30 19:17:00 -0400

    added chat widget to site

--- react-create-app/src/App.js
+++ react-create-app/src/App.js
@@ -1,28 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Widget } from 'react-chat-widget';
+
+import 'react-chat-widget/lib/styles.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Widget
+                    title="Real Bot"
+                    subtitle="Your real estate Agent"
+                   
+                />
+            </div>
+    )
+    }
 }
 
 export default App;
--- react-wp-real-estate-agent.php
+++ react-wp-real-estate-agent.php
@@ -12,7 +12,32 @@
  * @package         React_Wp_Real_Estate_Agent
  */
 
-function WREA_print_react_root_div(){
-	echo '<div class=""></div>';
-}
+function WREA_print_react_root_div(){ ?>
+	<style>
+		.rcw-conversation-container > .rcw-header {
+			/*background-color: red;*/
+		}
+
+		.rcw-message > .rcw-response {
+			background-color: black;
+			color: white;
+		}
+
+        .rcw-new-message {
+            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+            font-size: inherit;
+        }
+        .rcw-send {
+            padding: inherit;
+        }
+
+	</style>
+	<div id="root"></div>
+<?php }
 add_action('wp_footer', 'WREA_print_react_root_div');
+
+function RWRE_enqueue_scripts_styles() {
+	wp_enqueue_style( 'wp-react-agent', plugin_dir_url( __FILE__ )  . 'react-create-app/build/static/css/main.css' );
+	wp_enqueue_script( 'wp-react-agent', plugin_dir_url( __FILE__ ) . 'react-create-app/build/static/js/main.js' , [], '', true );
+}
+add_action( 'wp_enqueue_scripts', 'RWRE_enqueue_scripts_styles' );
