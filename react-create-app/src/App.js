486a83f21f74bfe1475d02adb0a45700295257e2
Be Broide <be.broide@gmail.com>
2019-03-31 14:32:08 -0400

    custom louncher

--- react-create-app/src/App.js
+++ react-create-app/src/App.js
@@ -1,16 +1,37 @@
 import React, { Component } from 'react';
-import { Widget } from 'react-chat-widget';
+import { Widget , addResponseMessage, isWidgetOpened, chatOpen } from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 
+import MaterialIcon, {colorPalette} from 'material-icons-react';
+
+const Launcher = ( handleToggle ) => {
+    return (
+        <button className={'rwre-toggle-button'} onClick={ () => { console.log('hi'); handleToggle() } }>
+            <MaterialIcon icon={  'chat'}  color='#fff' size='large' />
+        </button>
+    )
+};
+
 class App extends Component {
+    constructor(){
+        super();
+
+    }
+
+    componentDidMount() {
+        addResponseMessage("Welcome to this awesome chat!");
+    }
+
     render() {
         return (
             <div className="App">
                 <Widget
                     title="Real Bot"
                     subtitle="Your real estate Agent"
-                   
+                    fullScreenMode={true}
+                    launcher={Launcher}
+                    showCloseButton={true}
                 />
             </div>
     )
