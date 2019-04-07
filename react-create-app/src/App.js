26bc8dfa2831ec38159804262e53653f86ed7b65
Be Broide <be.broide@gmail.com>
2019-04-07 18:04:37 -0400

    checkboxes values from backend

--- react-create-app/src/App.js
+++ react-create-app/src/App.js
@@ -4,10 +4,20 @@ import CheckboxesResponse from './components/CheckboxesResponse'
 import 'react-chat-widget/lib/styles.css';
 
 
+// const data = {
+//     categories: ['Studio', '1 Bedroom', '2 Bedroom', '3 Bedroom', '4 Bedroom'],
+//     amenities: ['Rooftop', 'Doorman', 'Elevator', 'Laundry', 'TV', 'Gym', 'AC', 'Wifi', 'Balcony'],
+//     areas: ['Downtown', 'Midtown', 'East Village', 'Show', 'Upper East', 'Upper West']
+// };
+var bot_data = window.bot_data;
+const categories = bot_data.listing_category.map( category => category.cat_name );
+const amenities = bot_data.amenities.map( category => category.name );
+const areas =  bot_data.area.map( category => category.name );
+
 const data = {
-    categories: ['Studio', '1 Bedroom', '2 Bedroom', '3 Bedroom', '4 Bedroom'],
-    amenities: ['Rooftop', 'Doorma', 'Elevator', 'Laundry', 'TV', 'Gym', 'AC', 'Wifi', 'Balcony'],
-    areas: ['Downtown', 'Midtown', 'East Village', 'Show', 'Upper East', 'Upper West']
+    categories: categories,
+    amenities: amenities,
+    areas: areas
 };
 
 const App = () => {
