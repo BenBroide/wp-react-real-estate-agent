f74ac905413188ee03ed012d4c9fb3e496963c6f
Be Broide <be.broide@gmail.com>
2019-04-06 22:53:16 -0400

    selection works

--- react-create-app/src/App.js
+++ react-create-app/src/App.js
@@ -1,17 +1,203 @@
 import React, { Component } from 'react';
-import { Widget , addResponseMessage, isWidgetOpened, chatOpen } from 'react-chat-widget';
-
+import { Widget , toggleWidget, addResponseMessage, setQuickButtons, renderCustomComponent } from 'react-chat-widget';
+import Button from '@material-ui/core/Button';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
 import 'react-chat-widget/lib/styles.css';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
 
-import MaterialIcon, {colorPalette} from 'material-icons-react';
+import Chip from '@material-ui/core/Chip';
+import ViewIcon from '@material-ui/icons/Pageview';
+import Zoom from '@material-ui/core/Zoom';
 
-const Launcher = ( handleToggle ) => {
-    return (
-        <button className={'rwre-toggle-button'} onClick={ () => { console.log('hi'); handleToggle() } }>
-            <MaterialIcon icon={  'chat'}  color='#fff' size='large' />
-        </button>
-    )
-};
+const data = { categories : ['Studio', '1 Bedroom', '2 Bedroom', '3 Bedroom', '4 Bedroom'] , amenities : [ 'Rooftop', 'Doorma', 'Elevator', 'Laundry', 'TV', 'Gym', 'AC', 'Wifi', 'Balcony'] , areas : [ 'Downtown', 'Midtown', 'East Village', 'Show', 'Upper East', 'Upper West'] };
+
+
+class botDatesResponse extends Component {
+    constructor(){
+        super();
+        this.state = { dates : { startDate : '', endDate : '' }, renderedNext : false, defaultStart: '', defaultEnd : '' } ;
+    }
+
+    componentWillMount() {
+        var today = new Date();
+        var firstDayOfNextMonth = new Date(today.getFullYear(), today.getMonth()+1, 1).toISOString().split('T')[0];
+        var dateIn12Month = new Date(today.getFullYear(), today.getMonth()+13, 0).toISOString().split('T')[0];
+
+        this.setState( {
+            dates : { startDate : firstDayOfNextMonth, endDate: '' }
+        })
+    }
+
+    updateInquiry = ( event ) => {
+        console.log( event );
+        this.setState({ dates :  { startDate : event.val } })
+        if( !this.state.renderedNext ){
+            addResponseMessage( '3 Results found');
+            renderCustomComponent( ListingsList );
+            this.setState( { renderedNext : true });
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <form  noValidate>
+                <TextField
+                    id="date"
+                    label="Start Date"
+                    type="date"
+                    defaultValue={this.state.dates.startDate}
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                />
+                    <TextField
+                        id="date"
+                        label="End Date"
+                        type="date"
+                        onChange={ (event) => this.updateInquiry(event )}
+                        defaultValue={this.state.dates.endDate}
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                    />
+                </form>
+            </div>
+        )
+    }
+}
+
+class renderCheckboxes extends Component {
+    constructor(props){
+        super( props );
+        let selections = {};
+        Object.keys(data).map(  key => { selections[key] = []  } )
+        let renderedNext = {};
+        Object.keys(data).map(  key => { renderedNext[key] = false } )
+        this.state = { type : props.type ,listingCat : [], renderedNext : renderedNext , selections : selections } ;
+    }
+
+    getCheckboxes = ( type ) =>{
+        {return data[type].map( ( value , key ) =>
+                <FormControlLabel
+                    key={key}
+                    control={
+                        <Checkbox
+                            onChange={ () => this.updateInquiry( this.state.type , value)}
+                            value={value}
+                        />
+                    }
+                    label={value}
+                />
+            )}
+    }
+    updateInquiry = ( type, value ) => {
+
+        let selections = this.state.selections;
+        selections[type].push(value);
+        this.setState({ selections : selections })
+        if( !this.state.renderedNext[type] ){
+            addResponseMessage(`Please select ${type}. You can still modify the apartment size you want.`);
+            if( type === 'areas'){
+                renderCustomComponent(botDatesResponse);
+            } else {
+                renderCustomComponent( renderCheckboxes,  { type : type ==='categories' ? 'amenities' : 'areas' } );
+            }
+            this.setState( { renderedNext : {[type] : true} });
+        }
+    }
+
+    render() {
+
+        return (
+            <div>
+                <FormGroup row>
+                    {this.getCheckboxes( this.state.type  )}
+                </FormGroup>
+            </div>
+        )
+    }
+}
+
+
+class ListingsList extends Component {
+    constructor(){
+        super();
+
+    }
+
+    handleToggleListing = listing => () => {
+
+        console.log( listing );
+        // this.setState({
+        //     checked: newChecked,
+        // });
+    };
+
+    render() {
+        return (
+            <List dense >
+               {
+                   Array.from({length: 3}, (x,i) =>  {
+                       return <Zoom in={true} key={i}><ListItem style={ {maxWidth : '95%' , borderRadius : '10px', margin: '10px', background: '#f4f7f9'} } className={'rcw-response'} alignItems="flex-start"   button>
+                           <img style={ {maxWidth : '50px' } } src="/images/listing-1.jpg" />
+
+                           <ListItemText
+                               style={{margin: '5px 0'}}
+                               primary={`Listing ${i}`}
+                               secondary={
+                                   <React.Fragment >
+                                       <Chip
+                                           component={'span'}
+                                           label="East Village"
+                                       />
+                                       {data['amenities'].map((amenity, i) => {
+                                               return <Chip
+                                                   key={i}
+                                                   style={{margin: '5px'}}
+                                                   component={'span'}
+                                                   label={amenity}
+                                               />
+                                           })
+                                       }
+
+                                       <Fab
+                                           style={{margin: '10px 0'}}
+                                           variant="extended"
+                                           size="small"
+                                           color="primary"
+                                           aria-label="Add"
+                                       >
+                                           {/*<Icon>star</Icon>*/}
+                                           <ViewIcon style={{margin: '0 10px'}}/>
+                                           View Details
+                                       </Fab>
+                                    </React.Fragment>
+                               }
+                           />
+
+                           <ListItemSecondaryAction>
+                               <Checkbox
+                                   onChange={this.handleToggleListing(i)}
+                               />
+                           </ListItemSecondaryAction>
+
+                       </ListItem></Zoom>
+                   })
+               }
+            </List>
+        )
+    }
+}
 
 class App extends Component {
     constructor(){
@@ -19,19 +205,29 @@ class App extends Component {
 
     }
 
+    handleNewUserMessage = (newMessage) => {
+        renderCustomComponent( renderCheckboxes, { type : 'categories' } );
+        // Now send the message throught the backend API
+    }
+
+
     componentDidMount() {
-        addResponseMessage("Welcome to this awesome chat!");
+        toggleWidget();
+        addResponseMessage("Hi! Which apartment are you looking for?");
+        renderCustomComponent( renderCheckboxes, { type : 'categories' } );
+        //renderCustomComponent( ListingsList );
+
     }
 
     render() {
         return (
             <div className="App">
                 <Widget
-                    title="Real Bot"
+                    badge="2"
+                    title="Real Estate Bot"
                     subtitle="Your real estate Agent"
-                    fullScreenMode={true}
-                    launcher={Launcher}
                     showCloseButton={true}
+                    handleNewUserMessage={this.handleNewUserMessage}
                 />
             </div>
     )
--- react-wp-real-estate-agent.php
+++ react-wp-real-estate-agent.php
@@ -14,21 +14,33 @@
 
 function WREA_print_react_root_div(){ ?>
 	<style>
+        @import url('https://fonts.googleapis.com/css?family=Roboto');
+        .rcw-widget-container {
+            font-family: 'Roboto', sans-serif;
+        }
 		.rcw-conversation-container > .rcw-header {
-			/*background-color: red;*/
+			/*background-color: red; */
 		}
+
+        input[type="text"].rcw-new-message  {
+            font-size: 11px;
+            width: 100%;
+            border: 0;
+            background-color: #f4f7f9;
+            height: 30px;
+            padding-left: 15px;
+            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
+        }
+
         .rcw-full-screen .rcw-conversation-container {
             margin-bottom: 130px;
         }
 		.rcw-message > .rcw-response {
-			background-color: black;
+			background-color: blue;
 			color: white;
+
 		}
 
-        .rcw-new-message {
-            font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Oxygen,Ubuntu,Cantarell,Fira Sans,Droid Sans,Helvetica Neue,sans-serif;
-            font-size: inherit;
-        }
         .rcw-send {
             padding: inherit;
         }
@@ -43,8 +55,8 @@ function WREA_print_react_root_div(){ ?>
             right: 0;
             bottom: 0;
             position: absolute;
-            max-width: 100px;
-            border-radius: 75px;
+            /*max-width: 100px;*/
+            /*border-radius: 75px;*/
         }
 
 
@@ -55,6 +67,18 @@ add_action('wp_footer', 'WREA_print_react_root_div');
 
 function RWRE_enqueue_scripts_styles() {
 	wp_enqueue_style( 'wp-react-agent', plugin_dir_url( __FILE__ )  . 'react-create-app/build/static/css/main.css' );
+	wp_enqueue_style( 'material-ui', 'https://fonts.googleapis.com/icon?family=Material+Icons' );
+
+
 	wp_enqueue_script( 'wp-react-agent', plugin_dir_url( __FILE__ ) . 'react-create-app/build/static/js/main.js' , [], '', true );
 }
 add_action( 'wp_enqueue_scripts', 'RWRE_enqueue_scripts_styles' );
+
+function remove_unwanted_css() {
+	wp_dequeue_style( 'twentynineteen-style' );
+	wp_deregister_style( 'twentynineteen-style' );
+
+	//wp_dequeue_style( 'bootstrap-custom' );
+	//wp_deregister_style( 'bootstrap-custom' );
+}
+add_action( 'wp_enqueue_scripts', 'remove_unwanted_css', 20 );
